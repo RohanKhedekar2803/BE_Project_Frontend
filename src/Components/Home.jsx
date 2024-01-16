d69a9e8cb726c40e413401a5cb4e3fd26310ea1e
unknown <rohankhedekar2803@gmail.com>
2024-01-16 09:52:29 +0530

    folder structure and initial setup

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-xl text-red-800 font-bold underline">
+        Hello world!!!!!!
+      </h1>
+    </div>
+  )
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UsernameContext, UsernameProvider } from './Context/UsernameContext';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UsernameProvider>
+      <App />
+    </UsernameProvider>
+    
   </React.StrictMode>
 );
 
